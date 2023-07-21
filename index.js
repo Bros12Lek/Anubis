467d836f6db03815b8d0d89dd73d4392f89cbce6
otaviopacheco <134745956+otaviopacheco@users.noreply.github.com>
2023-07-21 16:34:58 -0300

    Add files via upload
    Página inicial criada [Otavio]

---
+++ index.js
@@ -0,0 +1,21 @@
+// let card = document.querySelector('.card__produtos');
+
+// function adicionaClasse(el,cl){
+//     el.classList.add(cl)
+// }
+
+
+
+// card.addEventListener('click', (Event) =>{
+//     card.classList.add('teste')
+//     console.log(card.className);
+    
+// })
+// card.addEventListener('click', (Event) =>{
+//     Element.classList.add('.teste')
+
+
+// } )
+
+
+
